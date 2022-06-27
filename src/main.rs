da6eed9d758d770bcdc27257be31f1816a869f9c
Éverton M. Vieira <emuvi@outlook.com.br>
2022-06-27 20:05:35 -0300

    setup a basic rust project

---
+++ src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("Qwiz (Qinpel Wizard) is a command program that features all the functionalities to download, build, install and execute multiple dynamic systems on the Qinpel architecture for the Power Intelligence platform.");
+}
